428ccae67219b16499d6f0b5076ba14dcb6a35d5
utpalrai <utpalkumar50@gmail.com>
2019-10-06 13:45:51 +0800

    version 0.1

--- push_heroku.sh
+++ push_heroku.sh
@@ -2,8 +2,8 @@
 pip freeze > requirements.txt
 # heroku create aws-management-test
 # git remote -v 
+# pip install gunicorn
 git add .
-# git add README.md environment.yml plot_shapefile.py correlation_plot.py app.py
 git commit -m "version 0.1.1"
 git push origin master
 git push heroku master
