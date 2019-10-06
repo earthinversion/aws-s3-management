18fbb7feb1c46836607ef2eaf6ac53f1779b1330
utpalrai <utpalkumar50@gmail.com>
2019-10-06 12:49:18 +0800

    version 0.1.1

--- push_heroku.sh
+++ push_heroku.sh
@@ -6,4 +6,4 @@ git add .
 # git add README.md environment.yml plot_shapefile.py correlation_plot.py app.py
 git commit -m "version 0.1.1"
 git push origin master
-# git push heroku master
+git push heroku master
