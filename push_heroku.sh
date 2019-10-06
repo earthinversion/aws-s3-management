d692d8efe03484c4843299819924cff64e24ca0e
utpalrai <utpalkumar50@gmail.com>
2019-10-06 12:40:42 +0800

    version 0.1

--- push_heroku.sh
+++ push_heroku.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 pip freeze > requirements.txt
+# heroku create aws-management-test
+# git remote -v 
 git add .
 # git add README.md environment.yml plot_shapefile.py correlation_plot.py app.py
 git commit -m "version 0.1"
 git push origin master
-# git push heroku master
+git push heroku master
