987c79a4b5ca5a5319b071add718b27b0067fca0
utpalrai <utpalkumar50@gmail.com>
2019-10-08 13:45:29 +0800

    version 0.2.1

--- push_heroku.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pip freeze > requirements.txt
-# heroku create aws-management-test
-# git remote -v 
-# pip install gunicorn
-git add .
-git commit -m "version 0.2"
-git push origin master
-# git push heroku master
